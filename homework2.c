a58d2f6cad1a7904d75f1414bc21925ff0649dba
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2023-09-22 16:24:25 -0400

    adding hnkkljljk

--- homework2.c
+++ homework2.c
@@ -2,9 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_IDENTIFIER_LENGTH 10
+#define MAX_ARRAY_LENGTH 100
+#define MAX_DIGITS_LENGTH 5
+#define MAX_CHARACTER_LENGTH 11
 
-/*
+typedef enum { 
+skipsym = 1, 
+identsym = 2, 
+numbersym = 3,  //....
+plussym = 4,  //....
+minussym = 5, //...
+multsym = 6, //.... 
+slashsym = 7, //.....
+ifelsym = 8,  
+eqlsym = 9, //...
+neqsym = 10, 
+lessym = 11, //...
+leqsym = 12, 
+gtrsym = 13, 
+geqsym = 14, 
+lparentsym = 15, 
+rparentsym = 16, 
+commasym = 17, 
+semicolonsym = 18, 
+periodsym = 19, 
+becomessym = 20, 
+beginsym = 21, 
+endsym = 22, 
+ifsym = 23, 
+thensym = 24, 
+whilesym = 25, 
+dosym = 26, 
+callsym = 27, 
+constsym = 28, 
+varsym = 29, 
+procsym = 30, 
+writesym = 31, 
+readsym = 32, 
+elsesym = 33
+} token_type;
+
+
+//   //symboles in pl0//
+//   // nulsym: 
+//   // idsym: vaiable names
 //   // numbersym:  
 //   // plussym: +
 //   // minussym: -
@@ -37,114 +78,85 @@
 //   // writelnsym: writeln
 //   // oddsym: odd
 //   // proceduresym: procedure
-*/
-typedef enum {
-  skipsym = 1,
-  identsym = 2,
-  numbersym = 3,
-  plussym = 4,  //....
-  minussym = 5, //.....
-  multsym = 6,  //
-  slashsym = 7, //
-  ifelsym = 8,
-  eqlsym = 9,
-  neqsym = 10,
-  lessym = 11,
-  leqsym = 12,
-  gtrsym = 13,
-  geqsym = 14,
-  lparentsym = 15,
-  rparentsym = 16,
-  commasym = 17,
-  semicolonsym = 18, //
-  periodsym = 19,
-  becomessym = 20,
-  beginsym = 21,
-  endsym = 22,
-  ifsym = 23,
-  thensym = 24,
-  whilesym = 25,
-  dosym = 26,
-  callsym = 27,
-  constsym = 28,
-  varsym = 29,
-  procsym = 30,
-  writesym = 31,
-  readsym = 32,
-  elsesym = 33
-} token_type;
+  
+//   //print geqsym
+//   printf("%d", becomessym);
+  
+  
+//   return 0;
+// }
 
-int tokenPrinter(char c) {
-  switch (c) {
-  case ';':
-    return semicolonsym;
-  case '+':
-    return plussym;
-  case '-':
-    return minussym;
-  case '*':
-    return multsym;
-  case '/':
-    return slashsym;
-  case '.':
-    return periodsym;
-  case '':
-    return;
-  case '':
-    return;
-  case '':
-    return;
-  case '':
-    return;
-  default:
-    return -1;
+
+int identify_element(ch){
+
+  if(ch >= 'a' && ch <= 'z'){}
+  else if(ch >= 'A' && ch <= 'Z'){}
+  else if(ch >= '0' && ch <= '9'){
+    return numbersym;
   }
+  else if(ch == '+'){
+    return plussym;
+  }
+  else if(ch == '-'){
+    return minussym;
+  }
+  else if(ch == '*'){
+    return multsym;
+  }
+  else if(ch == '/'){
+    return slashsym;
+  }
+  else if(ch == '='){
+    
+    
+    return eqlsym;
+    
+  }
+  else if(ch == '<'){
+    return lessym;
+  }
+
+
+    
+    
+  else if(ch == '_'){}
+  else if(ch == ){}
+  
+  
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("Error: Please include the file name.\n");
+int main( int argc, char *argv[]){
+
+  if(argc < 2){
+    printf("Error : please include the file name");
     return 1;
   }
-
+  
   FILE *fp = fopen(argv[1], "r");
 
-  if (fp == NULL) {
-    printf("Error: Cannot open file.\n");
-    return -1;
+  if(fp == NULL){
+    printf("Error : cannot open file");
+    return 1;
+  
   }
 
-  int currentIndex = 0;
-  int prevCharWasSpace = 0;
-
-  int c;
-  char original[500];
-
-  while ((c = fgetc(fp)) != EOF) {
-    if (currentIndex < 499) {
-      original[currentIndex] = c;
-      if (c == ' ' || c == '\t' || c == '\n') {
-        if (!prevCharWasSpace) {
-          file[currentIndex++] = ' ';
-          prevCharWasSpace = 1;
-        }
-      } else {
-        file[currentIndex++] = c;
-        prevCharWasSpace = 0;
-      }
-    } else {
-      printf("Error: Input exceeded 500 characters.\n");
-      break;
-    }
+  char arr[MAX_ARRAY_LENGTH];
+  int arrTracker = 0;
+  char c;
+  //read character input by fscanf
+  while(fscanf(fp, "%c", &c) != EOF){
+    //reads the file and store chacracter bt chacracter in the arr
+    arr[arrTracker] = c;
+    arrTracker++;
   }
+  //arrTracker is the last index of the array
+  for(int i = 0; i < arrTracker; i++){
+    printf("%c", arr[i]);
+  }
+  
+  
 
-  original[currentIndex] = '\0';
-  file[currentIndex] = '\0';
 
-  printf("Original string:\n%s\n", original);
-  printf("\n\n");
-  printf("Modified string:\n%s\n", file);
-
-  fclose(fp);
   return 0;
+  
 }
