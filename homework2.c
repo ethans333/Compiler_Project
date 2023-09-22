628a78016f2db3f8083af8f8a4c24db59d1a6d91
ethans333 <gamesniper321@gmail.com>
2023-09-22 14:19:51 -0400

    Added file reading

--- homework2.c
+++ homework2.c
@@ -49,8 +49,38 @@ int main(int argc, char *argv[]) {
 
   if (fp == NULL) {
     printf("Error : cannot open file");
-    return 1;
+    return -1;
   }
 
+  char inputArray[500];
+  int currentIndex =
+      0; // Initialize an index to track the current position in the array
+  int prevCharWasSpace = 0; // Flag to track consecutive white spaces
+
+  int c; // Variable to store the read character
+
+  while ((c = fgetc(fp)) != EOF) {
+    if (currentIndex < 500) { // Check if there's still space in the array
+      if (c == ' ' || c == '\t' || c == '\n') {
+        if (!prevCharWasSpace) {
+          inputArray[currentIndex++] =
+              ' '; // Replace consecutive white spaces with a single space
+          prevCharWasSpace = 1;
+        }
+      } else {
+        inputArray[currentIndex++] = c;
+        prevCharWasSpace = 0;
+      }
+    } else {
+      printf("Error: Input exceeded 500 characters.\n");
+      break;
+    }
+  }
+
+  inputArray[currentIndex] = '\0'; // Null-terminate the array
+
+  printf("Modified string:\n%s\n", inputArray);
+
+  fclose(fp);
   return 0;
 }
