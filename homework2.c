d5dea0889ab7c51d3cdb313e14552edf46a7727f
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2023-09-22 14:00:29 -0400

    Update homework2.c

--- homework2.c
+++ homework2.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
+
+typedef enum { skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, 
+multsym = 6,  slashsym = 7, ifelsym = 8,  eqlsym = 9, neqsym = 10, lessym = 11, leqsym = 12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17, semicolonsym = 18, periodsym = 19, becomessym = 20, 
+beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26, callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31, 
+readsym = 32, elsesym = 33
+} token_type;
+
+
+
 int main() {
 
   // Testing
